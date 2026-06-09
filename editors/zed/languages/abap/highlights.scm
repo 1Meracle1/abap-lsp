74a0c204b9309a6ee20fc94ac0d66c12e3753716
Ivan Bakhoryn <bahorin.ivan@gmail.com>
2026-06-09 13:47:14 +0200

    update tree-sitter grammar

--- editors/zed/languages/abap/highlights.scm
+++ editors/zed/languages/abap/highlights.scm
@@ -18,6 +18,50 @@
 (namespaced_identifier) @variable.special
 (cte_identifier) @variable.special
 (identifier) @variable
+((_
+  (keyword) @_type_keyword
+  .
+  (qualified_name (identifier) @type.builtin))
+  (#match? @_type_keyword "(?i)^TYPE$")
+  (#match? @type.builtin "(?i)^(c|n|d|t|i|int[1248]|f|p|decfloat(16|34)?|string|x|xstring|utclong|data|any|simple|numeric|clike|csequence|xsequence|object|char[0-9]+)$")
+  (#set! priority 110))
+((_
+  (keyword) @_type_keyword
+  .
+  (keyword) @_table_keyword
+  .
+  (keyword) @_of_keyword
+  .
+  (qualified_name (identifier) @type.builtin))
+  (#match? @_type_keyword "(?i)^TYPE$")
+  (#match? @_table_keyword "(?i)^TABLE$")
+  (#match? @_of_keyword "(?i)^OF$")
+  (#match? @type.builtin "(?i)^(c|n|d|t|i|int[1248]|f|p|decfloat(16|34)?|string|x|xstring|utclong|data|any|simple|numeric|clike|csequence|xsequence|object|char[0-9]+)$")
+  (#set! priority 110))
+((_
+  (keyword) @_type_keyword
+  .
+  (keyword) @_table_kind
+  .
+  (keyword) @_table_keyword
+  .
+  (keyword) @_of_keyword
+  .
+  (qualified_name (identifier) @type.builtin))
+  (#match? @_type_keyword "(?i)^TYPE$")
+  (#match? @_table_kind "(?i)^(ANY|INDEX|STANDARD|SORTED|HASHED)$")
+  (#match? @_table_keyword "(?i)^TABLE$")
+  (#match? @_of_keyword "(?i)^OF$")
+  (#match? @type.builtin "(?i)^(c|n|d|t|i|int[1248]|f|p|decfloat(16|34)?|string|x|xstring|utclong|data|any|simple|numeric|clike|csequence|xsequence|object|char[0-9]+)$")
+  (#set! priority 110))
+((constructor_expression
+  type: (identifier) @type.builtin)
+  (#match? @type.builtin "(?i)^(c|n|d|t|i|int[1248]|f|p|decfloat(16|34)?|string|x|xstring|utclong|data|any|simple|numeric|clike|csequence|xsequence|object|char[0-9]+)$")
+  (#set! priority 110))
+((constructor_expression
+  type: (qualified_name (identifier) @type.builtin))
+  (#match? @type.builtin "(?i)^(c|n|d|t|i|int[1248]|f|p|decfloat(16|34)?|string|x|xstring|utclong|data|any|simple|numeric|clike|csequence|xsequence|object|char[0-9]+)$")
+  (#set! priority 110))
 (escaped_identifier) @variable
 
 (operator) @operator
